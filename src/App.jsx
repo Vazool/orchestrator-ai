c0e8604874c3a6346bb9b727842c7071a4e28dc9
John Crook <johncharlescrook@outlook.com>
2026-01-30 14:23:55 +0000

    Add view state and buttons for screen navigation
    Code to allow a toggle between NotificationsScreen and OperatorScreen for running the demo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import NotificationsScreen from './components/NotificationsScreen'
+import { useState } from "react";
+import NotificationsScreen from "./components/NotificationsScreen";
+import OperatorScreen from "./components/OperatorScreen";
 
 function App() {
- 
+  const [view, setView] = useState("customer");
 
   return (
-    <NotificationsScreen/>
-  )
-    
+    <div>
+      <div>
+        <button onClick={() => setView("customer")}>
+          Customer View
+        </button>
+        <button onClick={() => setView("operator")}>
+          Operator Console
+        </button>
+      </div>
+
+      {view === "customer" && <NotificationsScreen />}
+      {view === "operator" && <OperatorScreen />}
+    </div>
+  );
 }
 
-export default App
+export default App;
